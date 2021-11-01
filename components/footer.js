e445eaf38edc262c67aefe6f9969900f7ec5b5d3
Peter Taylor <me@et1.uk>
2021-11-01 21:31:14 +0000

    Updated footer

--- components/footer.js
+++ components/footer.js
@@ -13,15 +13,15 @@ import Settings from '../settings.json';
         <div>
             <strong>&copy; URN 2021</strong><br/>
             University Radio Nottingham<br/>
-            <a href="https://urn1350.net/competition-terms/">Competition Terms</a><br/>
+            <a href="https://urn1350.net/pages/competitions/">Competition Terms</a><br/>
         </div>
         <div>
             <strong>Winner of Best Student Radio Station at the Student Radio Awards 2010, 2011, 2012, 2013, 2014, 2016,
                 2017 and 2018!</strong><br/>
             Contact the on-air presenters by messaging us on Instagram <a href="https://instagram.com/urn1350" target="_blank" rel="noopener">@URN1350</a>, or
             tweet <a href="https://twitter.com/urn1350" target="_blank" rel="noopener">@URN1350</a><br/>
-            Find out more about us, and get in touch on our <a href="https://urn1350.net/about-us/">about</a> page. 
-			<a href="https://urn1350.net/branding-pack/">Branding Pack</a> <br/>
+            Find out more about us, and get in touch on our <a href="https://urn1350.net/pages/about">About</a> page. 
+			<a href="https://urn1350.net/pages/branding-pack/">Branding Pack</a> <br/>
             Web Design &amp; Development by <a href="https://hacksoc.net">Hacksoc</a>
         </div>
     
